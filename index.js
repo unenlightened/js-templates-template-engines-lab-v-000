4eb9542ef8bc19e09a94ea87bcd306a1c30d71fd
Chely Ho <adept_inept@yahoo.com>
2018-07-08 15:32:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 function createPost() {
-  let title = document.getElementById("postTitle").value
-  let body = document.getElementById("postBody").value
-  let author = document.getElementById("postAuthor").value
+  let title = document.getElementById('postTitle').value
+  let body = document.getElementById('postBody').value
+  let author = document.getElementById('postAuthor').value
+  // expect(spy).toHaveBeenCalledWith(document.getElementById("page-template").innerHTML)
+  // expect(spy).toHaveBeenCalledWith(document.getElementById("post-template").innerHTML)
 
-  let postTemplate = document.getElementById("post-template").innerHTML
+  // let pageTemplate = document.getElementById('page-template').innerHTML
+  let postTemplate = document.getElementById('page-template').getElementById('post-template').innerHTML
   let templateFn = _.template(postTemplate)
 
   // it's inside of another template...
   let postDiv = document.getElementById('post')
 
-  // postDiv.innerHTML = templateFn({ 'title': title, 'body': body, 'author': author })
+  postDiv.innerHTML = templateFn({ 'title': title, 'body': body, 'author': author })
 }
 
 //issues with form grabbing a value?
 function postComment() {
-  var commenter = document.getElementById("commenterName").value;
-  // var comment = document.getElementById("commentText").value;
-  // var commentTemplate = document.getElementById("comment-template").innerHTML;
-  //
-  // var templateFn = _.template(commentTemplate);
-  //
-  // var commentsDiv = document.getElementById("comments");
-  //
-  // var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+  let commenter = document.getElementById('commenterName').value
+  let comment = document.getElementById('commentText').value
+  let commentTemplate = document.getElementById('comment-template').innerHTML
 
-  // commentsDiv.innerHTML += templateHTML;
+  let templateFn = _.template(commentTemplate)
+
+  let commentsDiv = document.getElementById('comments')
+
+  let templateHTML = templateFn({ 'comment': comment, 'commenter': commenter })
+
+  commentsDiv.innerHTML += templateHTML
 }
